34f38857527e775b21b6e1fe0dcf74897b17e9d5
Isaac True <isaac.true@gmail.com>
2015-02-26 11:59:40 +0300

    Implemented libnotify

--- kmnotify.c
+++ kmnotify.c
@@ -1,8 +1,11 @@
 #include <xcb/xcb.h>
 #include <stdio.h>
 #include <stdlib.h>
-//#include <libnotify/notify.h> 
+#include <libnotify/notify.h> 
 
+#define NOTIFY_TIMEOUT 1000
+
+void show_notification(const char * message);
 
 void register_events(xcb_connection_t * connection, xcb_window_t * root_window) {
     uint32_t mask               = XCB_CW_EVENT_MASK;
@@ -39,10 +42,11 @@ void get_keymap_name(xcb_connection_t * connection, char ** keymap_name) {
 }
 
 void handle_keymap_notify_event(xcb_connection_t * connection, xcb_generic_event_t * event) {
-    xcb_keymap_notify_event_t * km_event    = (xcb_keymap_notify_event_t *) event;
+//    xcb_keymap_notify_event_t * km_event    = (xcb_keymap_notify_event_t *) event;
     char * keymap_name = NULL;
     get_keymap_name(connection, &keymap_name);
     printf("Keymap change detected! New keymap: %s\n", keymap_name);
+    show_notification("Keyboard language changed!");
     free(keymap_name);
 }
 
@@ -58,7 +62,39 @@ void event_loop(xcb_connection_t * connection) {
     }
 }
 
+void initialise_libnotify() {
+    gboolean init_result = notify_init("kmnotify");
+    if (!init_result) {
+        fprintf(stderr, "Failed to initialise libnotify!\n");
+        return;
+    }   
+}
+
+void show_notification(const char * message) {
+    static NotifyNotification * notification = NULL;
+    if (notification == NULL) {
+        notification = notify_notification_new(NULL, NULL, NULL);
+    }
+    /* Close old notification and reuse */
+    notify_notification_close(notification, NULL);
+    notify_notification_update(notification, message, NULL, NULL);
+    notify_notification_set_urgency(notification, NOTIFY_URGENCY_LOW);
+    notify_notification_set_timeout(notification, NOTIFY_TIMEOUT);
+    GError * err = NULL;
+    gboolean show_result = notify_notification_show(notification, &err);
+    if (!show_result) {
+        fprintf(stderr, "ERROR: %s\n", err->message);
+    }
+}
+
+void deinitialise_libnotify() {
+    if (notify_is_initted()) {
+        notify_uninit();
+    }
+}
+
 int main(int argc, char * argv[]) {
+    initialise_libnotify();
     xcb_screen_t        * screen        = NULL;
     xcb_connection_t    * connection    = NULL;
     connection = xcb_connect( NULL, NULL );
@@ -66,7 +102,8 @@ int main(int argc, char * argv[]) {
     xcb_window_t root_window = screen->root;
     register_events(connection, &root_window);
     event_loop(connection);
-    xcb_disconnect (connection);
+    xcb_disconnect(connection);
+    deinitialise_libnotify();
     return 0;
 }
 
